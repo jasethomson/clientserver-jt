aac50f5c8fa127b04c2539de09939b761cbb8230
jasethomson <jasethomson31@gmail.com>
2019-08-06 15:19:27 -0700

    stored read and displayed data added data to server sent data through endpoints changed client address to correct server address added names to DOM

--- server/server.js
+++ server/server.js
@@ -1,2 +1,33 @@
 
+const express = require("express");
+const cors = require("cors");
+const server = express();
 
+const name = [{
+  "name": "jase"
+},{
+  "name": "nick"
+},{
+  "name": "Alex"
+}];
+
+const favFood = [{
+  "favFood": "burrito",
+  "ageDiscovered": 15
+}];
+
+
+
+server.use( cors() );
+
+server.get("/name", ( request, response )=>{
+  response.send(name);
+});
+
+server.get("/favFood", ( request, response )=>{
+  response.send(favFood);
+});
+
+server.listen(3001, () => {
+  console.log("Server loaded successfully");
+});
